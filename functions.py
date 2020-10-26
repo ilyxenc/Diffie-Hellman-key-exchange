7fcd5e87713abec354b1836ae33b1935e2686d65
Ilya Goldobin <ilyxenc@xryunya.local>
2020-10-26 11:30:50 +0300

    Add All

---
+++ functions.py
@@ -0,0 +1,80 @@
+import random
+import math
+
+# быстрый поиск модуля от степени числа
+def power(x, n, mod):
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        p = power(x, n / 2, mod)
+        return (p * p) % mod
+    else:
+        return (x * power(x, n - 1, mod)) % mod
+
+# генерация случайного числа фиксированной длины
+def rand(n):
+    rangeStart = 10 ** (n-1)
+    rangeEnd = (10 ** n) - 1
+    return random.randint(rangeStart, rangeEnd)
+
+# определение простое ли число
+def isPrime(num):
+    if (num < 2):
+        return False
+    for prime in LOW_PRIMES:
+        if (num == prime):
+            return True
+        if (num % prime == 0):
+            return False
+    return MillerRabin(num)
+
+# решето Эратосфена
+def primeSieve(sieveSize):
+    sieve = [True] * sieveSize
+    sieve[0] = False # Zero and one are not prime numbers.
+    sieve[1] = False
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
+
+# тест Миллера-Рабина
+def MillerRabin(num):
+    if num % 2 == 0 or num < 2:
+        return False
+    if num == 3:
+        return True
+    s = num - 1
+    t = 0
+    while s % 2 == 0:
+        s = s // 2
+        t += 1
+    for trials in range(5):
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1:
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
+    return True
+
+# генерация большого простого числа
+def randPrime(n):
+    rangeStart = 10 ** (n-1)
+    rangeEnd = (10 ** n) - 1
+    while True:
+        num = random.randint(rangeStart, rangeEnd)
+        if isPrime(num):
+            return num
+
+LOW_PRIMES = primeSieve(100)
